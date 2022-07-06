a04af1def59978a342b8faec5880ea4916e35b79
˜Thiago <thiago.sordi@mercadolivre.com>
2022-07-06 11:08:23 -0300

    First commit

---
+++ src/main/java/com/desafio_spring/desafio_spring/DesafioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.desafio_spring.desafio_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioSpringApplication.class, args);
+    }
+
+}
