067303c8a24d3daa15c23d6109288a477cf4387a
brunavottri <108009877+brunavottri@users.noreply.github.com>
2022-07-07 17:33:11 -0300

    Update src/main/java/com/desafio_spring/desafio_spring/repository/ProductRepo.java
    Co-authored-by: Pedro Thomé <pedro.almeida@mercadolivre.com>

--- src/main/java/com/desafio_spring/desafio_spring/repository/ProductRepo.java
+++ src/main/java/com/desafio_spring/desafio_spring/repository/ProductRepo.java
@@ -67,7 +67,7 @@ public class ProductRepo {
 
 
     public List<Product> getAllProductsByCategory(String category) {
-        List<Product> ProductsList = getAllProducts();
+        List<Product> productsList = getAllProducts();
         List<Product> productCategory = null;
         try {
             productCategory = ProductsList.stream()
