d2f0cf39c5b8754c1fbf80e301548004c8dcfd1c
pealmeida-meli <pedro.almeida@mercadolivre.com>
2022-07-07 17:08:13 -0300

    adiciona validação de produto inexistente

--- src/main/java/com/desafio_spring/desafio_spring/service/PurchaseService.java
+++ src/main/java/com/desafio_spring/desafio_spring/service/PurchaseService.java
@@ -1,13 +1,10 @@
 package com.desafio_spring.desafio_spring.service;
 
-import com.desafio_spring.desafio_spring.dto.PurchaseProductRequestDto;
 import com.desafio_spring.desafio_spring.dto.PurchaseResponseDto;
-import com.desafio_spring.desafio_spring.model.Purchase;
 import com.desafio_spring.desafio_spring.model.PurchaseProduct;
 
 import java.util.List;
 
 public interface PurchaseService {
-    List<Purchase> getAllPurchases();
     PurchaseResponseDto savePurchases(List<PurchaseProduct> products);
 }
--- src/main/java/com/desafio_spring/desafio_spring/service/PurchaseServiceImp.java
+++ src/main/java/com/desafio_spring/desafio_spring/service/PurchaseServiceImp.java
@@ -1,9 +1,7 @@
 package com.desafio_spring.desafio_spring.service;
 
-import com.desafio_spring.desafio_spring.dto.ProductDto;
 import com.desafio_spring.desafio_spring.dto.PurchaseProductResponseDto;
 import com.desafio_spring.desafio_spring.dto.PurchaseResponseDto;
-import com.desafio_spring.desafio_spring.dto.PurchaseProductRequestDto;
 import com.desafio_spring.desafio_spring.exception.ExceptionCustom;
 import com.desafio_spring.desafio_spring.model.Product;
 import com.desafio_spring.desafio_spring.model.Purchase;
@@ -17,7 +15,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
 
 @Service
 public class PurchaseServiceImp implements PurchaseService {
@@ -25,45 +22,44 @@ public class PurchaseServiceImp implements PurchaseService {
     private PurchaseRepo purchaseRepo;
 
     @Autowired
-    private ProductRepo productRepository;
+    private ProductRepo productRepo;
 
     @Override
-    public List<Purchase> getAllPurchases() {
-        return null;
-    }
-
-    @Override
-    public PurchaseResponseDto savePurchases(List<PurchaseProduct> products) {
-
+    public PurchaseResponseDto savePurchases(List<PurchaseProduct> purchaseProducts) {
         // Lógica para controle de estoque
-        Map<UUID, Product> map =
-                productRepository.getAllProducts().stream().collect(Collectors.toMap(Product::getProductId, item -> item));
-        for(PurchaseProduct p : products){
-            Product prod =  map.get(p.getProductId());
-            if(prod.getQuantity() >= p.getQuantity()){
-                prod.setQuantity(prod.getQuantity() - p.getQuantity());
-            }else{
-                throw new ExceptionCustom("Sem estoque");
+        Map<UUID, Product> productMap = productRepo.getAllProducts()
+                .stream()
+                .collect(Collectors.toMap(Product::getProductId, item -> item));
+
+        for (PurchaseProduct pp : purchaseProducts) {
+            Product product = productMap.get(pp.getProductId());
+            if (product == null) {
+                throw new ExceptionCustom("Produto(s) inexistente(s)");
+            }
+            if (product.getQuantity() >= pp.getQuantity()) {
+                product.setQuantity(product.getQuantity() - pp.getQuantity());
+            } else {
+                throw new ExceptionCustom("Sem estoque para o produto: " + product.getName());
             }
         }
 
         // Atualiza o registro de Produto no arquivo
-        products.stream().forEach(p -> productRepository.updateProduct(map.get(p.getProductId())));
-
-        Purchase purchase = new Purchase(UUID.randomUUID(), products);
-
+        purchaseProducts.forEach(p -> productRepo.updateProduct(productMap.get(p.getProductId())));
+        Purchase purchase = new Purchase(UUID.randomUUID(), purchaseProducts);
         purchaseRepo.savePurchase(purchase);
 
         // Soma o total da compra
-        DoubleStream productsTotalPrice = products
+        double total = purchaseProducts
                 .stream()
-                .mapToDouble(p -> productRepository.findById(p.getProductId()).getPrice() * p.getQuantity());
+                .mapToDouble(p -> productMap.get(p.getProductId()).getPrice() * p.getQuantity())
+                .sum();
 
-        return new PurchaseResponseDto(purchase.getId(),
-                products
+        return new PurchaseResponseDto(
+                purchase.getId(),
+                purchaseProducts
                         .stream()
-                        .map(x -> new PurchaseProductResponseDto(map.get(x.getProductId()), x.getQuantity()))
+                        .map(x -> new PurchaseProductResponseDto(productMap.get(x.getProductId()), x.getQuantity()))
                         .collect(Collectors.toList()),
-                productsTotalPrice.sum());
+                total);
     }
 }
